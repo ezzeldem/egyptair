2711b59fd10f5227fad3e5c11696a75a026152d4
ezzelden <ezzelden06@gmail.com>
2021-01-12 20:39:39 +0200

    eezz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
-    
 
+    // search box
+    $('.search-button').click(function(){
+        $('.search-box-pop').addClass('open-pop-up');
+        $('body').addClass('over-flow');
+    });
+    $('.search-box-pop .search-box-pop-form .close-pop-botton span').click(function(){
+        $('.search-box-pop').removeClass('open-pop-up');
+        $('body').removeClass('over-flow');
+    });
+
+    // niceSelect
+    $(".select-box").niceSelect();
+
+    
 });
